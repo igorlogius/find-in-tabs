624aa3bee00faba32ed57c157bc88cadc30981db
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-03-02 17:31:06 +0100

    feat: open search in tab

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
 browser.menus.create({
   title: "Open in Sidebar",
   contexts: ["browser_action"],
-  onclick: () => {
+  onclick: (tab, info) => {
     browser.sidebarAction.open();
+  },
+});
+
+browser.menus.create({
+  title: "Open in Tab",
+  contexts: ["browser_action"],
+  onclick: (tab, info) => {
+    browser.tabs.create({
+      url: "popup.html",
+    });
   },
 });
